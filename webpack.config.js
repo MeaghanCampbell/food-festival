f4a0687009ff7b3fdb0dafe90a6c7e9ff54c2877
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-04-20 20:50:33 -0500

    image webpack optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,53 @@
 // blueprint on the way webpack should behave in our project
 
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require('path')
 const webpack = require('webpack')
 
 module.exports = { 
     // where webpack looks to start building the module
-    entry: './assets/js/script.js', 
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     // tell where files are going to go and what are the names of the files
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
     },
+    module: {
+        rules: [
+          {
+            test: /\.jpg$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name(file) {
+                    return '[path][name].[ext]';
+                  },
+                  publicPath: function(url) {
+                    return url.replace('../', '/assets/');
+                  }
+                }
+              },
+              {
+                loader: 'image-webpack-loader'
+              }
+            ]
+          }
+        ]
+      },
     // play a role in directing what the webpack does, like tell it to use jquery we installed
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
         })
     ],
     // development or production
